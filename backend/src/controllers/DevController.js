b54cf649fc52dee5bd5fe21fdcaa05ab584da79b
Rafael Ctistiano <59890904+rafoccb@users.noreply.github.com>
2020-01-21 16:07:12 -0300

    Update DevController.js

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -3,13 +3,6 @@ const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
 const {findConnections, sendMessage } = require('../websocket');
 
-// index, show, store, update, destroy (geralmente nomes de funções)
-// index - mostrar uma lista
-// show - mostrar um unico
-// store = criar 
-// update - alterar
-// destroy - deletar
-
 module.exports = {
     async index(request, response) {
         const devs = await Dev.find();
@@ -24,9 +17,7 @@ module.exports = {
             // await para esperar a resposta
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
             const { name = login, avatar_url, bio } = apiResponse.data; 
-    
             const techsArray = parseStringAsArray(techs);
-    
             const location = {
                 type: 'Point',
                 coordinates: [longitude, latitude],
@@ -40,19 +31,13 @@ module.exports = {
                 techs: techsArray,
                 location,
             });
-
-            // Filtrar as conexões que estão a no máximo a 10km de distância
-            // e que o novo dev pelo menos uma das techs filtradas
-
-    
-            // console.log(name, avatar_url, bio, github_username);
+       
             const sendSocketMessageTo = findConnections(
                 { latitude, longitude },
                 techsArray,
             ); 
-            // console.log(sendSocketMessageTo);
             sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }  
-        return response.json(dev); // json não pode ser string, deve ser objeto ou array 
+        return response.json(dev); 
     }
 };
