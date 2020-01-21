cfa95dfaff6e03bba1248acc5e3b9a2f631c1795
Rafael Ctistiano <59890904+rafoccb@users.noreply.github.com>
2020-01-21 16:04:09 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,16 +3,9 @@ const DevController = require('./controllers/DevController');
 const SearchController = require('./controllers/SearchController');
 const routes = Router();
 
-// request = pega necessário do que foi enviado pelo cliente por exemplo
-//response = envia de volta para
-
-// Métodos HTTP: GET, POST PUT, DELETE
-// QUery Params: req.query (Filtros, ordenação, paginação...)
-// Route Params: req.params (Identificar um recurso na alteração ou remoção)
-// Body: req.body (Dados para criação ou alteração de um registro)
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
-// routes.post('/devsupdate', DevController.update);
+
 
 // rota busca
 routes.get('/search', SearchController.index);
